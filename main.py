5842dd92aacf8e89584b9c9dfb9229a1c055d938
ccatron88 <christophercatron@outlook.com>
2025-10-18 23:41:23 -0500

    Comment regarding usage of args

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
 def main():
-    print("Hello from ai-agent!")
+    load_dotenv()
+
+    messages = types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    args = sys.argv[1:]
+
+    # Check if user passed arguments
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+    user_prompt = " ".join(args)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", 
+        contents=messages
+        )
+    
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
+    print(response.text)
 
 
 if __name__ == "__main__":
