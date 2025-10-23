071d3edda30c23bc43d5d223da8bcab6efc839e6
ccatron88 <christophercatron@outlook.com>
2025-10-22 22:28:11 -0500

    Pushing WIP items

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -3,15 +3,23 @@ import os
 def get_files_info(working_directory, directory="."):
     file_path = os.path.join(working_directory, directory)
     absolute_path = os.path.abspath(file_path)
-    readme_is_dir = os.path.isdir((directory + '/README.md'))
+
+    print(working_directory)
+    print(directory)
+    print(file_path)
+    print(absolute_path)
 
 
-    if not file_path.startswith('/Users'):
+    if not absolute_path.startswith(file_path[:-2]):
         return (f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
+    else:
+        print(f"{file_path[:-2]} {absolute_path}")
     
-    if not is_dir:
-        return (f'Error: "{directory}" is not a directory')
+    return os.getcwd()
     
-    return (f"{} file_size={} is_dir={}")
+    # if not is_dir:
+    #     return (f'Error: "{directory}" is not a directory')
+    
+    # return (f"{} file_size={} is_dir={}")
 
 print (get_files_info((os.getcwd())))
