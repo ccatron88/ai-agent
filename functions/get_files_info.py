e00472e9fc311763272be3605343b4965f4db25b
ccatron88 <christophercatron@outlook.com>
2025-10-23 22:51:38 -0500

    refining function; working on try/except statements

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -3,23 +3,21 @@ import os
 def get_files_info(working_directory, directory="."):
     file_path = os.path.join(working_directory, directory)
     absolute_path = os.path.abspath(file_path)
+    is_dir = os.path.isdir(file_path)
+    size = os.path.getsize(file_path)
 
     print(working_directory)
     print(directory)
     print(file_path)
     print(absolute_path)
 
-
+    
     if not absolute_path.startswith(file_path[:-2]):
-        return (f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
-    else:
-        print(f"{file_path[:-2]} {absolute_path}")
+        return (f'Error: Cannot list "{directory}" as it is outside the permitted working directory')    
     
-    return os.getcwd()
+    if not is_dir:
+        return (f'Error: "{directory}" is not a directory')
     
-    # if not is_dir:
-    #     return (f'Error: "{directory}" is not a directory')
-    
-    # return (f"{} file_size={} is_dir={}")
+    return (f"{directory} file_size={size} is_dir={is_dir}")
 
 print (get_files_info((os.getcwd())))
