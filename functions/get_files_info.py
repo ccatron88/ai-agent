a49e3df845281ab7e127b9364ba309d8f610f4db
ccatron88 <christophercatron@outlook.com>
2025-10-25 23:18:36 -0500

    Refactored and finished code - tests are next.

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -3,21 +3,17 @@ import os
 def get_files_info(working_directory, directory="."):
     file_path = os.path.join(working_directory, directory)
     absolute_path = os.path.abspath(file_path)
-    is_dir = os.path.isdir(file_path)
+    is_dir = os.path.isdir(absolute_path)
     size = os.path.getsize(file_path)
-
-    print(working_directory)
-    print(directory)
-    print(file_path)
-    print(absolute_path)
-
     
-    if not absolute_path.startswith(file_path[:-2]):
-        return (f'Error: Cannot list "{directory}" as it is outside the permitted working directory')    
+    try:
+        absolute_path.startswith(file_path[:-2])
+    except:
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     
-    if not is_dir:
-        return (f'Error: "{directory}" is not a directory')
+    try:
+        os.path.isdir(absolute_path)
+    except:
+        return f'Error: "{directory}" is not a directory'
     
-    return (f"{directory} file_size={size} is_dir={is_dir}")
-
-print (get_files_info((os.getcwd())))
+    return f"{os.listdir(file_path)}: file_size={size} bytes, is_dir={is_dir}"
