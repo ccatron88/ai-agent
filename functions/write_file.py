4e9602e9cd4843ea59c5ccc03f7a3a685f11e6e8
ccatron88 <christophercatron@outlook.com>
2025-11-01 22:59:03 -0500

    Updated write_file function; not complete

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,10 +1,21 @@
 import os
 
-def write_file(working_directory, file_path, file):
-    working_abs = os.path.abs(working_directory)
+def write_file(working_directory, file_path, content):
+    working_abs = os.path.abspath(working_directory)
     full_path = os.path.join(working_abs, file_path)
 
-    if not full_path == working_abs or full_path.startswith(file_path):
+    if not (full_path == working_abs or full_path.startswith(working_abs)):
         return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
     
+    if not os.path.exists((full_path - '/' - file_path)):
+        os.makedirs(full_path)
+    else:
+        print(f'{working_abs} {file_path}')
+        return f'Error: File path could not be created'
     
+    try:
+        with open(file_path, "w") as f:
+            f.write(content)
+            return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except:
+        return f'Error: failed to write content to file'
