3d84e99049cbedcccd8f6687fee5d6359c46bcba
ccatron88 <christophercatron@outlook.com>
2025-11-03 21:47:07 -0600

    Working on file path check.

--- functions/write_file.py
+++ functions/write_file.py
@@ -3,14 +3,14 @@ import os
 def write_file(working_directory, file_path, content):
     working_abs = os.path.abspath(working_directory)
     full_path = os.path.join(working_abs, file_path)
+    print(full_path)
 
-    if not (full_path == working_abs or full_path.startswith(working_abs)):
+    if not (full_path == working_abs or full_path.startswith(working_abs + os.path.sep)):
         return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
     
-    if not os.path.exists((full_path - '/' - file_path)):
+    if not os.path.exists(full_path):
         os.makedirs(full_path)
     else:
-        print(f'{working_abs} {file_path}')
         return f'Error: File path could not be created'
     
     try:
