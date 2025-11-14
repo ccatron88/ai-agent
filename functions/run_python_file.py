e23763d1bb112b916384de375ff02765fa3c6274
ccatron88 <christophercatron@outlook.com>
2025-11-13 21:34:38 -0600

    Daily update

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -15,4 +15,4 @@ def run_python_file(working_directory, file_path, args=[]):
     if not full_path.endswith('.py'):
         return f'Error: "{file_path}" is not a Python file.'
 
-    subprocess.run(run_python_file, timeout=30, stdout=True, stderr=True)
+    subprocess.run(run_python_file, timeout=30, stdout=True, stderr=True, args=args)
