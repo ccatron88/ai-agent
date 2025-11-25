d12076a3c47223eb468c4e3a2efa033222a1148f
ccatron88 <christophercatron@outlook.com>
2025-11-24 22:49:14 -0600

    Daily update; nearly finished.

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -3,12 +3,9 @@ import subprocess
 
 def run_python_file(working_directory, file_path, args=[]):
     working_abs = os.path.abspath(working_directory)
-    abs_file_path = os.path.abspath(file_path)
     full_path = os.path.join(working_abs, file_path)
 
-    print(full_path.startswith(working_abs))
-
-    if not (full_path == working_abs or full_path.startswith(working_abs)):
+    if not full_path.startswith(working_abs):
         return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
     
     if not os.path.exists(full_path):
@@ -16,8 +13,22 @@ def run_python_file(working_directory, file_path, args=[]):
     
     if not full_path.endswith('.py'):
         return f'Error: "{file_path}" is not a Python file.'
+    
+    cmd = ['python', file_path] + args
 
-    result = subprocess.run(abs_file_path, timeout=30, capture_output=True, args=args)
+    try:
+        result = subprocess.run(cmd, cwd=working_abs, timeout=30, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return_string = f'STDOUT: {result.stdout}STDERR: {result.stderr}'.rstrip()
 
-    # print(result)
-    return result
+        
+        if result.returncode != 0:
+            return f'{return_string}\nProcess exited with code {result.returncode}'
+        if not result.stderr and not result.stdout:
+           return 'No output produced.'
+
+        return return_string
+    
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
+
+    
--- tests.py
+++ tests.py
@@ -4,4 +4,4 @@ from functions.write_file import write_file
 from functions.run_python_file import run_python_file
 
 # print(run_python_file("calculator", "main.py"))
-print(get_files_info("calculator", "."))
+print(run_python_file("calculator", "main.py"))
