89eba5aafc503297431ca439abf5a7f203e33968
ccatron88 <christophercatron@outlook.com>
2025-11-27 21:44:54 -0600

    Begin work on System Prompt

--- calculator/main.py
+++ calculator/main.py
@@ -6,6 +6,12 @@ from pkg.render import format_json_output
 
 
 def main():
+    system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+    response = client.models.generate_content(
+        model=model_name,
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
+    )
     calculator = Calculator()
     if len(sys.argv) <= 1:
         print("Calculator App")
