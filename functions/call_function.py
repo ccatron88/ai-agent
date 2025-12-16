61acbf171415688c2a214e28540126da96a71839
ccatron88 <christophercatron@outlook.com>
2025-12-15 21:39:41 -0600

    Daily update

--- functions/call_function.py
+++ functions/call_function.py
@@ -7,3 +7,5 @@ def call_function(function_call_part, verbose=False):
         print(f"Calling function: {function_call_part.name}({function_call_part.args})")
 
     print(f" - Calling function: {function_call_part.name}")
+
+    function_dict = {}
