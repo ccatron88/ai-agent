5ee5cf5b2be4111e3808a4b52d419d326f89d50f
ccatron88 <christophercatron@outlook.com>
2025-12-16 23:20:57 -0600

    Daily update

--- functions/call_function.py
+++ functions/call_function.py
@@ -1,6 +1,10 @@
 import os
 from google import genai
 from google.genai import types
+from functions.get_file_content import get_file_content
+from functions.get_files_info import get_files_info
+from functions.run_python_file import run_python_file
+from functions.write_file import write_file
 
 def call_function(function_call_part, verbose=False):
     if verbose:
@@ -8,4 +12,11 @@ def call_function(function_call_part, verbose=False):
 
     print(f" - Calling function: {function_call_part.name}")
 
-    function_dict = {}
+    functions_dict = {
+        "get_file_content": get_file_content,
+        "get_files_info": get_files_info,
+        "run_python_file": run_python_file,
+        "write_file": write_file
+    }
+
+    
