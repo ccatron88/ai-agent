d9acc0950bb2f8b25af5eb3969601fac7f67837c
ccatron88 <christophercatron@outlook.com>
2026-01-10 22:27:45 -0600

    Daily update

--- functions/call_function.py
+++ functions/call_function.py
@@ -29,12 +29,12 @@ def call_function(function_call, verbose=False):
     # Still need to fix this
     # Import or change the 
     # values of the keys?
-    function_map = {
-        "get_file_content": get_file_content,
-        "get_files_info": get_files_info,
-        "run_python_file": run_python_file,
-        "write_file": write_file,
-    }
+    # function_map = {
+    #     "get_file_content": get_file_content,
+    #     "get_files_info": get_files_info,
+    #     "run_python_file": run_python_file,
+    #     "write_file": write_file,
+    # }
     
 
     if function_call.name not in available_functions:
