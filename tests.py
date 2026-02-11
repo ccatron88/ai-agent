74637796348388ca4f359b88021f591e47003f1f
ccatron88 <christophercatron@outlook.com>
2026-02-10 23:33:57 -0600

    Daily update

--- tests.py
+++ tests.py
@@ -2,11 +2,12 @@ from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
 from functions.write_file import write_file
 from functions.run_python_file import run_python_file
+from functions.call_function import call_function
 
 # print(run_python_file("calculator", "main.py"))
-print(run_python_file("calculator", "main.py"))
-print(run_python_file("calculator", "main.py", ["3 + 5"]))
-print(run_python_file("calculator", "tests.py"))
-print(run_python_file("calculator", "../main.py"))
-print(run_python_file("calculator", "nonexistent.py"))
-print(run_python_file("calculator", "lorem.txt"))
+print(call_function("calculator", "main.py"))
+print(call_function("calculator", "main.py", ["3 + 5"]))
+print(call_function("calculator", "tests.py"))
+print(call_function("calculator", "../main.py"))
+print(call_function("calculator", "nonexistent.py"))
+print(call_function("calculator", "lorem.txt"))
